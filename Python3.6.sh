27df9864166534aac3ebec46dd564b76f671ba59
mne <39185579+mneyapo@users.noreply.github.com>
2019-10-17 15:37:27 +0200

    Add files via upload

---
+++ Python3.6.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Installer Python3.6 sur un Raspberry Pi"
+sudo apt-get install python3-dev libffi-dev libssl-dev -y
+wget https://www.python.org/ftp/python/3.6.3/Python-3.6.3.tar.xz
+tar xJf Python-3.6.3.tar.xz
+cd Python-3.6.3
+./configure
+make
+sudo make install
+sudo pip3 install --upgrade pip
+$SHELL
